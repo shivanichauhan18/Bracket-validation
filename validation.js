f5b1770ecb5fa0c5c95c70df9bd1d2d8074cc5d5
Alka05 <alka19@navgurukul.org>
2019-11-25 16:00:03 +0530

    bracket validation web page with edge case

--- validation.js
+++ validation.js
@@ -15,6 +15,7 @@ app.get("/validation",(req,res)=>{
 
 app.post("/new",(req,res)=>{
     var str1="{,[,("
+
     var str2="},],)"
     
     var user_name=req.body.bracket;
@@ -37,7 +38,41 @@ app.post("/new",(req,res)=>{
 
 })
 
+app.post("/new1",(req,res)=>{
+    var brackets=req.body.bracket;
+    let isMatchingBrackets = function (str) {
+        let stack = [];
+        let map = {
+            '(': ')',
+            '[': ']',
+            '{': '}'
+        }
+    
+        for (let i = 0; i < str.length; i++) {
+    
+            // If character is an opening brace add it to a stack
+            if (str[i] === '(' || str[i] === '{' || str[i] === '[' ) {
+                stack.push(str[i]);
+            }
+            //  If that character is a closing brace, pop from the stack, which will also reduce the length of the stack each time a closing bracket is encountered.
+            else {
+                let last = stack.pop();
+    
+                //If the popped element from the stack, which is the last opening brace doesn’t match the corresponding closing brace in the map, then return false
+                if (str[i] !== map[last]) {return false};
+            }
+        }
+        // By the completion of the for loop after checking all the brackets of the str, at the end, if the stack is not empty then fail
+            if (stack.length !== 0) {return false};
+    
+        return true;
+    }
+    var data=isMatchingBrackets(brackets)
+    res.send(data)
 
+    
+    console.log(data);
+})
 
 app.listen(8000,()=>{
     console.log("8000 pr sunta hai....")
